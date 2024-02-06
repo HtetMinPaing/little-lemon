4614eb9e59100ce506f803438045dd55b9750482
Your Name~ <hmphtetminpaing@gmail.com>
2024-02-06 20:49:35 +0630

    Adding the components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import About from './components/About';
+import Bottom from './components/Bottom';
+import Hero from './components/Hero';
+import Highlight from './components/Highlight';
+import Navigation from './components/Navigation';
+import Testimonials from './components/Testimonials';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      <Hero />
+      <Highlight />
+      <Testimonials />
+      <About />
+      <Bottom />
     </div>
   );
 }
