6d557e8edeff239a6cef2c01e0e49e01c66c21ec
Your Name~ <hmphtetminpaing@gmail.com>
2024-02-13 15:11:44 +0630

    Add login page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import "../styles/modules/pages/loginPage.scss"
 
 const LoginPage = () => {
+  const form = [
+    { type: "text", label: "First_Name" },
+    { type: "text", label: "Last_Name" },
+    { type: "email", label: "Email" },
+    { type: "password", label: "Password" }
+  ]
   return (
-    <div>LoginPage</div>
+    <div className='login_page'>
+      <h1>LogIn to Little Lemon</h1>
+      <div className='login_form_wrapper'>
+        {
+          form.map((input) => {
+            return (<InputField type={input.type} label={input.label} key={input} />)
+          })
+        }
+        <div className='login_button_wrapper'>
+          <button className='login_button'>Log In</button>
+          <button className='signup_button'>Sign Up</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const InputField = ({ type, label }) => {
+  return (
+    <div className='inputField'>
+      <label htmlFor={label}>{label}</label>
+      <input id={label} value={label} type={type} />
+    </div>
   )
 }
 
